2fa9cf66459923b9eca3358f9fd6f7b14e23438d
Sivabharathy <gotocva@gmail.com>
2020-08-05 17:11:59 +0530

    flexbox layout basics

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 // import { LoginComponent } from "./login/login.component";
 // import { RegisterComponent } from "./register/register.component";
@@ -36,6 +37,10 @@ const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   { path: '**', redirectTo: '' },
 ];
